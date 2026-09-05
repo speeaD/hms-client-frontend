04c4bbcce356a95ca00e33b36150551b9a908597
speeaD <sejiofor20@gmail.com>
2026-09-05 13:09:59 +0100

    update rooms

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,5 +3,5 @@ import { ROOMS } from "@/data/rooms";
 
 export default async function Home() {
   const rooms = await ROOMS();
-    return <HotelExperience rooms={rooms as [typeof rooms[number]]} />;
+    return <HotelExperience rooms={rooms} />;
 }
